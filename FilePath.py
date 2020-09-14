cb15519941a181554e5e94ee059ef78e39768663
5knnbdwm <samuel.soeller@gmx.de>
2020-09-14 14:56:22 +0200

    general cleanup

--- FilePath.py
+++ FilePath.py
@@ -3,54 +3,56 @@ import random
 
 # log/cups/
 def get_path_part(sFilename):
-    if len(sFilename) > 0 and sFilename[len(sFilename) - 1] == '/':
+    if len(sFilename) > 0 and sFilename[len(sFilename) - 1] == "/":
         return sFilename
 
     try:
-        #integer is index where the last slash is found
-        integer = int(sFilename.rindex('/'))
+        # integer is index where the last slash is found
+        integer = int(sFilename.rindex("/"))
     except:
         integer = -1
 
-    dirName = ''
+    dirName = ""
     if integer >= 0:
-        dirName = sFilename[0: integer + 1]
+        dirName = sFilename[0 : integer + 1]
     else:
-        dirName = ''
+        dirName = ""
 
     return dirName
+
 
 # access_log
 def getFilenamePart(sFilename):
     try:
-        int(sFilename.rindex('/'))
+        int(sFilename.rindex("/"))
     except:
         return sFilename
 
-    pos = sFilename.rindex('/')
-    base_name = sFilename[pos + 1:]
+    pos = sFilename.rindex("/")
+    base_name = sFilename[pos + 1 :]
     return base_name
 
 
-#.png
+# .png
 def getEndOfFile(sFilename):
     try:
-        occurrences = [m.start() for m in re.finditer('\.', sFilename)]
-        return sFilename[occurrences[-1] + 1:]
+        occurrences = [m.start() for m in re.finditer("\.", sFilename)]
+        return sFilename[occurrences[-1] + 1 :]
     except:
         pass
 
-    return ''
+    return ""
 
 
-assert(get_path_part("log/cups/access_log") == "log/cups/")
-assert(get_path_part("log/cups/") == "log/cups/")
-assert(get_path_part("cups/access_log") == "cups/")
-assert(get_path_part("access_log") == "")
-assert(getFilenamePart("log/cups/access_log") == "access_log")
-assert(getFilenamePart("log/cups/") == "")
-assert(getFilenamePart("cups/access_log") == "access_log")
-assert(getFilenamePart("access_log") == "access_log")
-assert(getEndOfFile("log/cups/access_log") == "")
-assert(getEndOfFile("base/FileHelper.cpp") == "cpp")
-assert(getEndOfFile("base/FileHelper.cpp.bak") == "bak")
+assert get_path_part("log/cups/access_log") == "log/cups/"
+assert get_path_part("log/cups/") == "log/cups/"
+assert get_path_part("cups/access_log") == "cups/"
+assert get_path_part("access_log") == ""
+assert getFilenamePart("log/cups/access_log") == "access_log"
+assert getFilenamePart("log/cups/") == ""
+assert getFilenamePart("cups/access_log") == "access_log"
+assert getFilenamePart("access_log") == "access_log"
+assert getEndOfFile("log/cups/access_log") == ""
+assert getEndOfFile("base/FileHelper.cpp") == "cpp"
+assert getEndOfFile("base/FileHelper.cpp.bak") == "bak"
+
